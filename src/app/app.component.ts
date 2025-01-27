26226659d2278761458749cf072f5e5e3069973c
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2025-01-27 21:15:20 +0800

    added sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseListComponent } from './features/expenses/components/expense-list/expense-list.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ExpenseListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
