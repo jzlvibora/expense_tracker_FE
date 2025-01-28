824078ddb7825179a02dc37d79dcf81ad3693223
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2025-01-28 11:19:04 +0800

    configure routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ExpenseListComponent } from './features/expenses/components/expense-list/expense-list.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ExpenseListComponent],
+  imports: [RouterOutlet,RouterLink, ExpenseListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
